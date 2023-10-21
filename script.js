38e4a7d1597493e8b13d10ce8ab87de45bf542fe
Pallavi Biswas <64264921+pallavibiswas@users.noreply.github.com>
2023-10-21 17:46:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:3,
+               
+            },
+            1000:{
+                items:3,
+                
+            }
+        }
+    }) 
+})
